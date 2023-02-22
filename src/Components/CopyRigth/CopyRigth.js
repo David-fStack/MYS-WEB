cdab418e1c8be40bc205903a09f61069b0877a14
David-fStack <davidmaximilianomoyano@gmail.com>
2023-02-21 22:57:06 -0300

    se crea CopyRigth

--- src/Components/CopyRigth/CopyRigth.js
+++ src/Components/CopyRigth/CopyRigth.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './CopyRigth.css'
 
 const CopyRigth = () => {
+    const currentYear = new Date().getFullYear();
 
     return (
-        <div class="copyRigth">© Copyright 2022 - All Rights Reserved - 
+        <div class="copyRigth">© Copyright {currentYear}
+            - All Rights Reserved -
             <a href="https://github.com/David-fStack">David-fStack</a>
-            
         </div>
-        
-        
     );
 };
 
